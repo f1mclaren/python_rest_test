fb4e528cea96cd139cdfce87e297ab1c2f796597
Dukchang (Dennis) Hwang <dennis.bicf@gmail.com>
2021-10-06 06:05:22 +0900

    Fixed the prefix of the DB url if the DB is PostgreSQL.

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 db_url = os.getenv('DATABASE_URL', 'sqlite:///data.db')
 
-if db_url.startswith('postgres:'):
-    db_url.replace('postgres:', 'postgresql:')
+if db_url.startswith('postgres://'):
+    db_url.replace('postgres://', 'postgresql://', 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
