0c0c335ccfd238d852f5196d69c3a9becfdb008f
Dukchang (Dennis) Hwang <dennis.bicf@gmail.com>
2021-10-06 06:55:25 +0900

    Fixed the prefix of the DB url if the DB is PostgreSQL.

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ import sys
 from app import app
 from db import db
 
-sys.stdout.write(app.config['SQLALCHEMY_DATABASE_URI'])
 
 db.init_app(app)
 
 
 @app.before_first_request
 def create_tables():
+    sys.stdout.write(app.config['SQLALCHEMY_DATABASE_URI'])
     db.create_all()
