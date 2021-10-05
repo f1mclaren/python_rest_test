c0eb7e1755a78c675fb60770c6cdf151b2e84752
Dukchang (Dennis) Hwang <dennis.bicf@gmail.com>
2021-10-06 06:13:33 +0900

    Fixed the prefix of the DB url if the DB is PostgreSQL.

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ db_url = os.getenv('DATABASE_URL', 'sqlite:///data.db')
 if db_url.startswith('postgres://'):
     db_url.replace('postgres://', 'postgresql://', 1)
 
-print(db_url)
-sys.stdout.flush()
+sys.stdout.write(db_url)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
